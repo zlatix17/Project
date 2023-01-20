726b2186831cd8ee18d6eeb2a77f9c28b6be7aae
Martin-Nedkov <107184278+Martin-Nedkov@users.noreply.github.com>
2023-01-20 11:06:32 +0200

    proekt

---
+++ MyFirstASP/MyFirstASP/Controllers/BaiController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyFirstASP.Controllers
+{
+    public class BaiController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
